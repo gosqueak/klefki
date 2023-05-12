94404d60b1ed9a2cbe522cd72abd65d0ec315b43
rebeljah <rebeljah@gmail.com>
2023-05-12 00:53:28 -0400

    added leader config

--- cmd/main.go
+++ cmd/main.go
@@ -5,28 +5,28 @@ import (
 
 	kit "github.com/gosqueak/apikit"
 	"github.com/gosqueak/jwt"
+	"github.com/gosqueak/leader/team"
 	"github.com/gosqueak/jwt/rs256"
 	"github.com/gosqueak/klefki/api"
 	"github.com/gosqueak/klefki/database"
 )
 
-const (
-	Addr            = "0.0.0.0:8083"
-	AuthServerUrl   = "http://0.0.0.0:8081"
-	JwtKeyPublicUrl = AuthServerUrl + "/jwtkeypub"
-	AudIdentifier   = "ECDHSERVICE"
-)
+
 
 func main() {
+	tm := team.Download("https://raw.githubusercontent.com/gosqueak/leader/main/Teamfile.json")
+	klefki := tm["klefki"]
+	steelix := tm["steelix"]
+
 	db := database.Load("data.sqlite")
 
-	pKey, err := kit.Retry[*rsa.PublicKey](3, rs256.FetchRsaPublicKey, []any{JwtKeyPublicUrl})
+	pKey, err := kit.Retry[*rsa.PublicKey](3, rs256.FetchRsaPublicKey, steelix.Url + "/jwtkeypub")
 	if err != nil {
 		panic("could not fetch RSA key")
 	}
 
-	aud := jwt.NewAudience(pKey, AudIdentifier)
-	serv := api.NewServer(Addr, db, aud)
+	aud := jwt.NewAudience(pKey, klefki.JWTInfo.AudienceName)
+	serv := api.NewServer(klefki.Url, db, aud)
 
 	serv.Run()
 }
