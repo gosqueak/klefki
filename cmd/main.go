7fff73caeb4b4bdf76c1fda9ddce07558f106cb5
rebeljah <rebeljah@gmail.com>
2023-05-15 23:29:46 -0400

    created dev env using docker and leader

--- cmd/main.go
+++ cmd/main.go
@@ -12,8 +12,6 @@ import (
 	"github.com/gosqueak/leader/team"
 )
 
-
-
 func main() {
 	tm := team.Download(os.Getenv("TEAMFILE_JSON_URL"))
 	klefki := tm["klefki"]
@@ -21,13 +19,13 @@ func main() {
 
 	db := database.Load("data.sqlite")
 
-	pKey, err := kit.Retry[*rsa.PublicKey](3, rs256.FetchRsaPublicKey, steelix.Url + "/jwtkeypub")
+	pKey, err := kit.Retry[*rsa.PublicKey](3, rs256.FetchRsaPublicKey, steelix.Url.String()+"/jwtkeypub")
 	if err != nil {
 		panic("could not fetch RSA key")
 	}
 
 	aud := jwt.NewAudience(pKey, klefki.JWTInfo.AudienceName)
-	serv := api.NewServer(klefki.Url, db, aud)
+	serv := api.NewServer(klefki.ListenAddress, db, aud)
 
 	serv.Run()
 }
